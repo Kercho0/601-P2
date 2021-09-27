de0853957d9803bd00bb5dc7aacb792fcc05f45f
Kercho0 <70703656+Kercho0@users.noreply.github.com>
2021-09-26 21:25:10 -0400

    Update searchparty.py

--- searchparty.py
+++ searchparty.py
@@ -4,29 +4,29 @@ import tweepy
 from datetime import datetime
 
 def searchparty(keyword, date, max_number):
-    for tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang="en", until=date, tweet_mode='extended').items(max_number):
-        num=0
-        num=num+1
-        hashtags = tweet.entities['hashtags']
-        try:
-            text = tweet.retweeted_status.full_text
-        except AttributeError:
-            text=tweet.full_text
-        hashtext=list()
-        for j in range(0, len(hashtags)):
-            hashtext.append(hashtags[j]['text'])
+	num=0
+	for tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang="en", until=date, tweet_mode='extended').items(max_number):
+		num=num+1
+		hashtags = tweet.entities['hashtags']
+		try:
+		    text = tweet.retweeted_status.full_text
+		except AttributeError:
+		    text=tweet.full_text
+		hashtext=list()
+		for j in range(0, len(hashtags)):
+		    hashtext.append(hashtags[j]['text'])
 
-        print()
-        print(f"Tweet {num}: ")
-        print(f"Username: {tweet.user.screen_name}")
-        print(f"Description: {tweet.user.description}")
-        print(f"Location: {tweet.user.location}")
-        print(f"Following Count: {tweet.user.friends_count}")
-        print(f"Follower Count: {tweet.user.followers_count}")
-        print(f"Total Tweets: {tweet.user.statuses_count}")
-        print(f"Retweet Count: {tweet.retweet_count}")
-        print(f"Tweet Text: {text}")
-        print(f"Hashtags Used: {hashtext}")
+		print()
+		print(f"Tweet {num}: ")
+		print(f"Username: {tweet.user.screen_name}")
+		print(f"Description: {tweet.user.description}")
+		print(f"Location: {tweet.user.location}")
+		print(f"Following Count: {tweet.user.friends_count}")
+		print(f"Follower Count: {tweet.user.followers_count}")
+		print(f"Total Tweets: {tweet.user.statuses_count}")
+		print(f"Retweet Count: {tweet.retweet_count}")
+		print(f"Tweet Text: {text}")
+		print(f"Hashtags Used: {hashtext}")
   
       
 if __name__ == '__main__':   
@@ -51,6 +51,8 @@ if __name__ == '__main__':
 # https://docs.tweepy.org/en/stable/api.html#tweets
 # https://docs.tweepy.org/en/stable/client.html#tweets
 #
+
+
 
 
 
